728fc7293351fd13f101e17a309b746d241cfefe
Newton Kobia <newtonmutethia100@gmail.com>
2024-05-24 13:34:53 +0300

    A broad tutorial lesson on classes in JS

---
+++ Classes/listupdate/index.js
@@ -0,0 +1,43 @@
+class ListElement {
+    constructor (element) {
+        this.listElements = element;
+        this.listItems = ['Home','Land'];
+    }
+
+    static createElement (text) {
+        const createLi = document.createElement('li');
+
+        createLi.textContent = text;
+
+        return createLi;
+    }
+
+    // Removes default elements from the listItems array
+    update () {
+        while (this.listElements.firstChild) {
+            this.listElements.removeChild(this.listElements.firstChild)
+        }
+
+        for (const text of this.listItems) {
+            this.listElements.appendChild(ListElement.createElement(text))
+        }
+    }
+
+    add (text) {
+        this.listItems.push(text)
+        this.update();
+    }
+
+    remove (index) {
+        this.listItems.splice(index, 1)
+        this.update();
+    }
+}
+
+
+const myList = document.getElementById('myList');
+const List = new ListElement(myList);
+
+
+
+
